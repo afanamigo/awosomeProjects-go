0ded58a9c22bd4096346b26b0c6849616dda757c
afanamigo <afanamigo@gmail.com>
2025-08-13 18:14:58 +0800

    Update Hello.go

--- main/Hello.go
+++ main/Hello.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // go project is based on package(folder), save as java but grace and much more easy
-// running with cli: go run filename.go or go build, then sh executable file.
+// running with cli: go run filename.go or go build, then run the executable file.
 // in this case is, go run hello.go, Or go build Hello.go, ./Hello
 func main() {
 	fmt.Println("Hello World")
