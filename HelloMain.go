36de51ee891479b944fb48eca4407e87de529c3b
mini <afanamigo@gmail.com>
2025-08-14 22:32:40 +0800

    update HelloMain.go

---
+++ HelloMain.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// go project is based on package(folder), save as java but grace and much more easy
+// running with cli: go run filename.go or go build, then run the executable file.
+// in this case is, go run hello.go, Or go build HelloMain.go, ./Hello
+
+// copy from Hello.go, package main not means under main Folder.
+// main is the program source code package, entry point of the executable application.
+func main() {
+	fmt.Println("Hello World, Hello Golang!")
+}
--- main/Hello.go
+++ main/Hello.go
@@ -2,9 +2,10 @@ package main
 
 import "fmt"
 
-// go project is based on package(folder), save as java but grace and much more easy
+// go project is based on package(src folder level)
+// save as java but grace and much more easy, didn't force to use folder package level(folder is package in java)
 // running with cli: go run filename.go or go build, then run the executable file.
-// in this case is, go run hello.go, Or go build Hello.go, ./Hello
+// in this case is, go run Hello.go, Or go build Hello.go, ./Hello
 func main() {
 	fmt.Println("Hello World, Hello Golang!")
 }
